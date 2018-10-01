9da9903d296b978c896cb27a74bcd360d137d245
dreadwail <belakey@microsoft.com>
2018-09-30 17:19:29 -0700

    First pass at creating a main menu. Not reachable yet because no input handling is taking place.

--- src/engine.ts
+++ src/engine.ts
@@ -1,7 +1,7 @@
 import { Color, DisplayMode, Engine } from 'excalibur';
 
 import { createLoader } from './loader';
-import { Map, Splash } from './scenes';
+import { MainMenu, Map, Splash } from './scenes';
 
 const engineOptions = {
   width: 800,
@@ -16,15 +16,18 @@ export const start = (): Promise<void> => {
   return new Promise((resolve, reject) => {
     const engine = new Engine(engineOptions);
 
-    const map = new Map(engine);
-    engine.add(Map.sceneName, map);
-
     const splash = new Splash(engine);
     engine.add(Splash.sceneName, splash);
+
+    const mainMenu = new MainMenu(engine);
+    engine.add(MainMenu.sceneName, mainMenu);
+
+    const map = new Map(engine);
+    engine.add(Map.sceneName, map);
 
     const loader = createLoader();
     engine.start(loader).then(resolve, reject);
 
-    engine.goToScene('splash');
+    engine.goToScene(Splash.sceneName);
   });
 };
